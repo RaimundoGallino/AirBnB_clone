b9f99e75db950d513d6cbd919d8527d105ea4541
RaimundoGallino <raigallino0@gmail.com>
2021-06-24 17:17:41 -0300

    Upload WIP files

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,30 @@
-
 #!/usr/bin/python3
 '''file that defines the Base_model class'''
 
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
 
     id = ''
     created_at = ''
     update_at = ''
+    name = ''
+    my_number = ''
 
-    def __init__(self):
-        self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
+    def __init__(self, *args, **kwargs):
 
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.update_at = datetime.now()
+        else:
+            for k, v in kwargs.items():
+                if (k == "created_at" or k == "updated_at"):
+                    setattr(self, k, v)
+                else:
+                    setattr(self, k, v)
+            
     def __str__(self):
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
@@ -24,5 +32,6 @@ class BaseModel:
         self.update_at = datetime.now()
 
     def to_dict(self):
+        dict = {'my_number': self.my_number, 'name': self.name, '__class__': type(self).__name__, 'updated_at': self.update_at.isoformat(), 'id': self.id , 'created_at': self.created_at.isoformat()}
 
-        return {"'name': '{}', '__class__': '{}', 'updated_at': '{}', 'id': '{}', 'created_at': '{}'".format(self.name, type(self).__name__, self.update_at, self.id, self.created_at)}
+        return dict
