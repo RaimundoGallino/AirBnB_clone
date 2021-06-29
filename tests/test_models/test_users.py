9e94c5a61e2d226a92f1001613edcb5228ce7ae8
Diego Varela <2806@holbertonschool.com>
2021-06-29 15:42:01 -0300

    Adding test case for users

---
+++ tests/test_models/test_users.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Unitest for Users'''
+import unittest
+from models.users import User
+
+
+class TestBase(unittest.TestCase):
+    '''Test Cases'''
+
+    def test_User(self):
+        '''Test'''
+        b = User()
+        b.email = "Test"
+        self.assertEqual(b.email, "Test")
+
+
+if __name__ == "__main__":
+    unittest.main()
