fb821208d18034ae4e42ded495cbf19d0cc1558d
RaimundoGallino <raigallino0@gmail.com>
2021-06-30 20:01:18 -0300

    Updating test_save_method adding def

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -24,6 +24,24 @@ class TestBase(unittest.TestCase):
         self.assertTrue(hasattr(user, "created_at"))
         self.assertTrue(hasattr(user, "updated_at"))
 
+    def test_pub_attrs(self):
+
+        user = User()
+        self.assertTrue(hasattr(user, "email"))
+        self.assertEqual(user.email, "")
+        self.assertTrue(hasattr(user, "password"))
+        self.assertEqual(user.password, "")
+        self.assertTrue(hasattr(user, "first_name"))
+        self.assertEqual(user.first_name, "")
+        self.assertTrue(hasattr(user, "last_name"))
+        self.assertEqual(user.last_name, "")
+
+        new_d = user.to_dict()
+        self.assertEqual(type(new_d), dict)
+        for attr in user.__dict__:
+            self.assertTrue(attr in new_d)
+            self.assertTrue("__class__" in new_d)
+
 
 if __name__ == "__main__":
     unittest.main()
