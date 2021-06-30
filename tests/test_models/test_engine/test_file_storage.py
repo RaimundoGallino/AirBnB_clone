b51ee07ff3b9768695a0fec96902481dbbbd35fc
RaimundoGallino <raigallino0@gmail.com>
2021-06-30 18:36:04 -0300

    Updating test_save_method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,6 +12,10 @@ import pep8
 class TestFileStorage(unittest.TestCase):
     '''Test Cases'''
 
+    def test_docstring(self):
+        """module docstring length"""
+        self.assertTrue(len(FileStorage.__doc__) >= 1)
+
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
